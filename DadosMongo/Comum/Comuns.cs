8064af5c1833131ce653ee4369add5315166d531
Gustavo <gustavo@engsolutions.com.br>
2015-06-29 19:34:59 -0300

    c

---
+++ DadosMongo/Comum/Comuns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DadosMongo.Comum
+{
+    public static class Comuns
+    {
+        public static string GetDescription(this Type type)
+        {
+            var descriptions = (DescriptionAttribute[])
+                type.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return descriptions.Length == 0
+                ? null
+                : descriptions[0].Description; 
+        }
+    }
+}
