6b0677d57cfcc8218625bfc4f8dc9284a89938b2
Gustavo <gustavo@engsolutions.com.br>
2015-06-29 19:34:50 -0300

    C

---
+++ DadosMongo/AcessaDados/ConexaoBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using DadosMongo.Comum;
+using DadosMongo.Entidades;
+using MongoDB.Driver;
+
+namespace DadosMongo.AcessaDados
+{
+    public class ConexaoBase<T> where T : BaseEntity
+    {
+        private readonly MongoCollection _collection;
+
+        public ConexaoBase()
+        {
+            var server = new MongoClient(ConfigurationManager.AppSettings["LocalBd"]).GetServer();
+
+            _collection = server
+                .GetDatabase(ConfigurationManager.AppSettings["BdName"])
+                .GetCollection(typeof(T).GetDescription());
+        }
+
+        public MongoCollection GetCollection()
+        {
+            return _collection;
+        }
+    }
+}
---
+++ DadosMongo/Entidades/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace DadosMongo.Entidades
+{
+    public class BaseEntity
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime DataCad { get; set; }
+    }
+}
