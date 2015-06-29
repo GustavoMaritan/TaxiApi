260e9c2e917a1e4c237b91858beeb57320b18d70
Gustavo <gustavo@engsolutions.com.br>
2015-06-29 19:42:32 -0300

    global

--- TaxiApi/Global.asax.cs
+++ TaxiApi/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using System.Threading;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -10,11 +12,17 @@ namespace TaxiApi
     {
         protected void Application_Start()
         {
-            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes); 
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
         }
     }
 }
+
