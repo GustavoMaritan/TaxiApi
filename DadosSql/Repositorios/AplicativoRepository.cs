2b47e34683c7ee482da5f030a85a1f2562600c97
Gustavo <gustavo@engsolutions.com.br>
2015-07-09 09:23:29 -0300

    Com

---
+++ DadosSql/Repositorios/AplicativoRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DadosSql.DataModel;
+
+namespace DadosSql.Repositorios
+{
+    public class AplicativoRepository
+    {
+        public List<CoperativaApp> GetAll()
+        {
+            using (var ct = new Contexto.Contexto())
+            {
+                return ct.DbCoperativa
+                    .Select(x => new CoperativaApp
+                    {
+                        Id = x.Id,
+                        Descricao = x.Descricao,
+                        Bairro = x.Bairro,
+                        Telefones = x.Telefones.Select(y => new TelefoneApp
+                        {
+                            Ddd = y.Ddd,
+                            Numero = y.Numero
+                        }).ToList()
+                    }).ToList();
+            }
+        }
+    }
+}
--- DadosSql/Repositorios/CoperativaRepository.cs
+++ DadosSql/Repositorios/CoperativaRepository.cs
@@ -7,20 +7,6 @@ namespace DadosSql.Repositorios
 {
     public class CoperativaRepository : BaseRepository<Coperativa>
     {
-        public new List<CoperativaApp> GetAll()
-        {
-            return Ct.DbCoperativa
-                .Select(x => new CoperativaApp
-                {
-                    Id = x.Id,
-                    Descricao = x.Descricao,
-                    Bairro = x.Bairro,
-                    Telefones = x.Telefones.Select(y => new TelefoneApp
-                    {
-                        Ddd = y.Ddd,
-                        Numero = y.Numero
-                    }).ToList()
-                }).ToList();
-        }
+       
     }
 }
